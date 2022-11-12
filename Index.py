770b4f09a22fe0ab1f322c17c297d75b7b89a272
Charlie <cbaldini@uci.edu>
2022-11-12 14:15:44 -0800

    fix regex bug, adjust content loop, fix generator bug, add comments

--- Index.py
+++ Index.py
@@ -9,7 +9,7 @@ import sys
 
 STEMMER = stem.PorterStemmer()
 BATCH_SIZE = 3000000  # bytes
-PATH = "DEV/"
+PATH = "C:/SearchEngine/developer/DEV/"
 PARTIALINDEXPATH = "partialIndices/partialIndex_"
 FULLINDEXPATH = "full_Index/index.jsonl"
 
@@ -27,28 +27,32 @@ class BuildIndex:
 
     def buildIndex(self):
         docNumber = 0  # Initial document number
-        path = Path(self.webPagesPath)  # one folder while testing
-        for dir in path.iterdir():
+        path = Path(self.webPagesPath)
+        for dir in path.iterdir(): # iterate over all directories and their corresponding files
             for file in dir.iterdir():
                 with open(file) as readFile:
-                    jsonObj = json.load(readFile)
+                    fileDict = json.load(readFile)
 
-                    url = urldefrag(jsonObj["url"]).url  # defragged
+                    url = urldefrag(fileDict["url"]).url  # defragged
                     if url in self.urlIDMap:
                         continue
-                    self.urlIDMap[url] = docNumber
+                    self.urlIDMap[url] = docNumber  # map docID and url both ways
                     self.IDUrlMap[docNumber] = url
 
-                    rawContent = jsonObj["content"]
+                    rawContent = fileDict["content"]
                     content = BeautifulSoup(rawContent, features="html.parser")
                     content = content.find_all()
+                    token_frequency = Counter()
                     for line in content:
-                        tokens = [STEMMER.stem(word) if "'" not in word else STEMMER.stem(word.replace("'", ''))
-                                  for word in re.sub(r"[^a-zA-Z0-9']", " ", line.text.lower()).split()]
-                        token_frequency = Counter(tokens)
-                        for token, frequency in token_frequency.items():
-                            if token not in self.invertedIndex:
-                                self.invertedIndex[token] = list()
+                        if not line.text:
+                            continue
+                        tokens = [STEMMER.stem(word) for word in
+                                  re.sub(r"[^a-zA-Z0-9\s]", "", line.text.lower()).split()]
+                        token_frequency.update(tokens)
+                    for token, frequency in token_frequency.items():
+                        if token not in self.invertedIndex:
+                            self.invertedIndex[token] = [(docNumber, frequency)]
+                        else:
                             self.invertedIndex[token].append((docNumber, frequency))
                 self.filesProcessed += 1
 
@@ -56,7 +60,6 @@ class BuildIndex:
                 if sys.getsizeof(self.invertedIndex) >= BATCH_SIZE:
                     self.writePartialIndexesToFile()
 
-            print(dir)
         if self.invertedIndex:
             self.writePartialIndexesToFile()
 
@@ -76,8 +79,8 @@ class BuildIndex:
             sortedIndex = sorted(self.invertedIndex.items(), key=lambda x: x[0])
             self.invertedIndex = {}
             for k, v in sortedIndex:
-                keyValAsJson = json.dumps({k: v})
-                f.write(keyValAsJson + "\n")
+                keyValAsJson = json.dumps({k: v})  # convert to single json line
+                f.write(keyValAsJson + "\n")  # write json
         self.partialFileIndex += 1
         self.partialIndexFiles.append(path)
 
@@ -92,45 +95,44 @@ class BuildIndex:
     def mergeFiles(self):
         """ opens main index and all partial indices, sorts and writes all partial indices into main """
 
-        with open(FULLINDEXPATH, 'w') as final: # open final index
-            generators = [self.generateLine(n) for n in self.partialIndexFiles]  # generator obj for each partial
+        with open(FULLINDEXPATH, 'w') as final:  # open final index
+            fileGenerators = [self.generateLine(n) for n in self.partialIndexFiles]  # generator obj for each partial
             nextWords = []  # stores most recent word yielded from each generator
-            toBePopped = set()  # pops off generator when empty
+            toBeRemoved = set()  # stores empty generators queued for removal
 
-            for i, gen in enumerate(generators):
-                currMapping = list(next(gen).items())
-                nextWords.append((currMapping[0][0], currMapping[0][1], generators[i]))  # store k,v pair w/ gen obj
+            for i, gen in enumerate(fileGenerators):
+                currGensNext = list(next(gen).items())
+                nextWords.append((currGensNext[0][0], currGensNext[0][1], fileGenerators[i]))  # store k,v pair w/ gen obj
 
-            while generators:
+            while fileGenerators:  # loop until all generators empty
                 nextWords.sort(key=lambda x: x[0])  # sort yielded words
-                getNextVals = [nextWords[0][2]]  # initialize list to get next value from generator
-                merged = nextWords[0][1]  # initialize values that may be merged if there is a word match
+                getNextVals = [nextWords[0][2]]  # list of generators that need to yield new value
+                postings = nextWords[0][1]  # initialize postings of min nextWord, may merge if there is a match later
                 i = 1
-                while i < len(nextWords) - 1:
+                while i < len(nextWords):
                     if nextWords[i][0] == nextWords[i - 1][0]:
-                        merged.extend(nextWords[i][1])  # same word, merge values
+                        postings.extend(nextWords[i][1])  # same word, merge postings
                         getNextVals.append(nextWords[i][2])
                     else:
                         break
                     i += 1
 
                 self.tokensProcessed += 1
-                keyValAsJson = json.dumps({nextWords[i - 1][0]: merged})  # write k,v pair into final index
+                keyValAsJson = json.dumps({nextWords[i - 1][0]: postings})  # write k,v pair into final index
                 final.write(keyValAsJson + "\n")
                 nextWords = nextWords[len(getNextVals):]  # remove written word
 
                 for i, gen in enumerate(getNextVals):
                     try:
-                        currMapping = list(next(gen).items())
-                        nextWords.append((currMapping[0][0], currMapping[0][1], gen))  # store new k,v pair w/ gen obj
+                        currGensNext = list(next(gen).items())
+                        nextWords.append((currGensNext[0][0], currGensNext[0][1], gen))  # store new k,v pair w/ gen obj
                     except StopIteration:
-                        toBePopped.add(gen)  # queue generator for removal as it's empty
-                generators = [gen for gen in generators if gen not in toBePopped]
-                toBePopped = set()
+                        toBeRemoved.add(gen)  # queue empty generator for removal
+                fileGenerators = [gen for gen in fileGenerators if gen not in toBeRemoved]
+                toBeRemoved = set()
 
             for file in self.partialIndexFiles:  # remove partial indices
                 Path(file).unlink()
-
 
 
 if __name__ == "__main__":
