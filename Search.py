7ac559b3b537f68383e1806ee4c9b48bdb9cbd12
Yaseen Khan <yaseenk@uci.edu>
2022-11-16 13:27:16 -0800

    Search Starter Code

--- Search.py
+++ Search.py
@@ -1,38 +1,63 @@
 
-def booleanResults(word_search):
-  # gets all the pages the term is stored at 
-  pass
+# gets all the postings the term is stored at 
+def booleanResults(word_search, inverted_index):
+  postings = list(inverted_index[word_search])
+  return postings
 
 
-def getSearchResults(user_query):
+def getSearchResults(user_query, inverted_index):
   unique_words = set(user_query.split())
-
-  # gets all the pages with the unique results
+  search_words = set()
+  
+  # for each word, check if the term exists in the index
   for word in unique_words:
-    booleanResults(word)
+    if word in inverted_index:
+      search_words.add(word)
+
+  oneWordMatches = set()
+
+  # gets all the pages where at least one word from the query matches
+  for word in search_words:
+    getPostings = booleanResults(word, inverted_index)
+    for posting in getPostings:
+      if posting not in oneWordMatches:
+        oneWordMatches.add(posting)
+
+  best_results = set()
+
+  # filter out results that do not contain all the query matches
+  for posting in oneWordMatches:
+    allTerms = True
+    for word in search_words:
+      postings = inverted_index[word]
+      if posting not in postings:
+        allTerms = False
+        break
+
+      # if doc contains all words, add it to the results
+      if allTerms:
+        best_results.add(posting)
+      
+  return best_results
 
 
-  # for AND results, the result URLS must contain all pages
-  results = []
+# Retrieves and prints 5 URLS for each query
+def printTopURLS(results):
   if len(results) == 0:
-    print("No results found")
-
-  return results
-
-
-# Retrieves and prints the Top 5 URL
-def printTopURLS():
-  pass
+    print("No Results Found")
+  else:
+    print(results)
 
 
 def main():
   user_query = input("Enter a query: ").strip()
 
+  # get the inverted_index from the 
+  inverted_index = {}
+
   while user_query != "":
-    print(user_query)
-    new_words = user_query.split()
-    for word in new_words:
-      print(word)
+    results = getSearchResults(user_query, inverted_index)
+    printTopURLS(results)
     user_query = input("Enter a query: ").strip()
 
 
