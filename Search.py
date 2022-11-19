dbb856d545b58e9284e8ab12d2f488681ea71219
Charlie <cbaldini@uci.edu>
2022-11-18 21:07:16 -0800

    add comments

--- Search.py
+++ Search.py
@@ -40,43 +40,45 @@ class SearchEngine:
         wordFrequency = Counter()
         searchWords = [STEMMER.stem(word) for word in
                        re.sub(r"[^a-zA-Z0-9\s]", "", userQuery.lower()).split()]
-        wordFrequency.update(searchWords)
-        unstoredWords = set()
-        wordPostings = dict()
+        wordFrequency.update(searchWords)  # keep count for words in query to calc tf
+        unstoredWords = set()  # set of words that don't exist in index to be popped
+        wordPostings = dict()  # dict of words and their corresponding posting tuples
 
-        for word in wordFrequency.keys():
+        for word in wordFrequency.keys():  # iterate through words, find pointer in index, add words/postings to dict
             wordOffsetInIndexFile = self.loadData.getWordPositionInIndex(word)
             if wordOffsetInIndexFile is not None:
                 wordPostings[word] = self.loadData.getPosting(wordOffsetInIndexFile, word)
             else:
                 unstoredWords.add(word)
 
-        for word in unstoredWords:
+        for word in unstoredWords:  # remove all words not present in index
             wordFrequency.pop(word)
 
         incrementer = 0
-        docVectors = dict()
-        queryVector = []
-        vectorLen = len(wordFrequency.keys())
+        docVectors = dict()  # store tfidf vector for each document
+        queryVector = []  # query tfidf vector
+        vectorLen = len(wordFrequency.keys())  # get query vector len to initialize docVectors to that size
         for word, postings in wordPostings.items():
-            for docID, tfidf in postings:
+            for docID, tfidf in postings:  # initialize and update doc vectors
                 if docID not in docVectors:
                     docVectors[docID] = [0 if val != incrementer else tfidf for val in range(vectorLen)]
                 else:
                     docVectors[docID][incrementer] = tfidf
             incrementer += 1
+            # update query vector
             queryVector.append(round((1 + log10(wordFrequency[word])) * (log10(FILESPROCESSED/len(postings))), 2))
         # print(f"doc vectors ->  {docVectors}")
         # print(f"query vector ->  {queryVector}")
 
         cosineSim = []
-        for docID, docVector in docVectors.items():
+        for docID, docVector in docVectors.items():  # compute cosine similarity
             cosineSim.append((docID, dot(queryVector, docVector)/(norm(queryVector) * norm(docVector))))
-        cosineSim.sort(key=lambda x: x[1], reverse=True)
-        #print(f"cosine sim ->  {cosineSim[:5]}")
-        return [self.loadData.IDUrlMap[str(doc[0])] for doc in cosineSim[:5]]
+        cosineSim.sort(key=lambda x: x[1], reverse=True)  # sort from high to low
+        # print(f"cosine sim ->  {cosineSim[:5]}")
+        return [self.loadData.IDUrlMap[str(doc[0])] for doc in cosineSim[:5]]  # return top 5 docs
 
-    def top5Results(self, results, type=""):
+    @staticmethod
+    def top5Results(results, type=""):
         print(type)
         for i, docID in enumerate(results[:5]):
             print(f"{i+1}. {docID}")
@@ -90,7 +92,7 @@ def main():
         booleanResults = searchEngine.booleanSearch(user_query)
         cosineSimResults = searchEngine.cosineSimilarity(user_query)
         searchEngine.top5Results(booleanResults, "boolean results")
-        searchEngine.top5Results(cosineSimResults, "cosine similarity results ")
+        searchEngine.top5Results(cosineSimResults, "cosine similarity results")
         user_query = input("Enter a query: ").strip()
 
 
