1dbbb6adba4dd8c3c9810e4e4d209e8a40cd0eb2
Charlie <cbaldini@uci.edu>
2022-11-18 20:55:55 -0800

    implement cosineSimilarity, modify print format

--- LoadData.py
+++ LoadData.py
@@ -1,13 +1,14 @@
 import json
 from BTrees.OOBTree import OOBTree
 
+
 class LoadData:
     def __init__(self):
         self.wordIndexTree = OOBTree()
         with open("IDUrlMap.json", 'r') as URLmap:
             self.IDUrlMap = json.load(URLmap)
         self.createWordBTree()
-    
+
     def createWordBTree(self) -> None:
         """stores words and their corresponding place in the main index in a btree """
         with open("full_index/index.jsonl", "r") as index:
@@ -16,12 +17,11 @@ class LoadData:
                 jsonObj = json.loads(line)
                 self.wordIndexTree.insert(list(jsonObj.keys())[0], offset)
                 offset += len(line) + 1
-    
+
     def getWordPositionInIndex(self, word: str) -> int:
         """takes a word as an argument and returns its position in the main index file"""
 
         return self.wordIndexTree[word] if word in self.wordIndexTree else None
-
 
     def getPosting(self, offset: int, word: str) -> list:
         """finds word in index using offset, loads json, returns list of postings"""
--- Search.py
+++ Search.py
@@ -1,17 +1,25 @@
 from LoadData import LoadData
 from nltk import stem
 import re
+from collections import Counter
+from math import log10
+from numpy import dot
+from numpy.linalg import norm
+
+
 
 STEMMER = stem.PorterStemmer()
+FILESPROCESSED = 53792
+
 
 class SearchEngine:
     def __init__(self):
         self.loadData = LoadData()
 
-    def booleanSeach(self, userQuery):
+    def booleanSearch(self, userQuery: str):
         # same transformation is applied on input query string
         searchWords = [STEMMER.stem(word) for word in
-                                  re.sub(r"[^a-zA-Z0-9\s]", "", userQuery.lower()).split()]
+                       re.sub(r"[^a-zA-Z0-9\s]", "", userQuery.lower()).split()]
         searchWords = set(searchWords)
         documentsSet = set()
 
@@ -28,21 +36,63 @@ class SearchEngine:
                     documentsSet = documentsSet.intersection(docs)
         return [self.loadData.IDUrlMap[str(doc)] for doc in documentsSet]
 
-    def printTop5Results(self, results):
-      i = 0
-      for docID in results:
-        print(self.loadData.IDUrlMap[str(docID)])
-        i += 1
-        if i == 5: break
+    def cosineSimilarity(self, userQuery: str):
+        wordFrequency = Counter()
+        searchWords = [STEMMER.stem(word) for word in
+                       re.sub(r"[^a-zA-Z0-9\s]", "", userQuery.lower()).split()]
+        wordFrequency.update(searchWords)
+        unstoredWords = set()
+        wordPostings = dict()
+
+        for word in wordFrequency.keys():
+            wordOffsetInIndexFile = self.loadData.getWordPositionInIndex(word)
+            if wordOffsetInIndexFile is not None:
+                wordPostings[word] = self.loadData.getPosting(wordOffsetInIndexFile, word)
+            else:
+                unstoredWords.add(word)
+
+        for word in unstoredWords:
+            wordFrequency.pop(word)
+
+        incrementer = 0
+        docVectors = dict()
+        queryVector = []
+        vectorLen = len(wordFrequency.keys())
+        for word, postings in wordPostings.items():
+            for docID, tfidf in postings:
+                if docID not in docVectors:
+                    docVectors[docID] = [0 if val != incrementer else tfidf for val in range(vectorLen)]
+                else:
+                    docVectors[docID][incrementer] = tfidf
+            incrementer += 1
+            queryVector.append(round((1 + log10(wordFrequency[word])) * (log10(FILESPROCESSED/len(postings))), 2))
+        # print(f"doc vectors ->  {docVectors}")
+        # print(f"query vector ->  {queryVector}")
+
+        cosineSim = []
+        for docID, docVector in docVectors.items():
+            cosineSim.append((docID, dot(queryVector, docVector)/(norm(queryVector) * norm(docVector))))
+        cosineSim.sort(key=lambda x: x[1], reverse=True)
+        #print(f"cosine sim ->  {cosineSim[:5]}")
+        return [self.loadData.IDUrlMap[str(doc[0])] for doc in cosineSim[:5]]
+
+    def top5Results(self, results, type=""):
+        print(type)
+        for i, docID in enumerate(results[:5]):
+            print(f"{i+1}. {docID}")
+        print()
 
 
 def main():
     searchEngine = SearchEngine()
     user_query = input("Enter a query: ").strip()
     while user_query != "":
-        results = searchEngine.booleanSeach(user_query)
-        searchEngine.printTop5Results(results)
+        booleanResults = searchEngine.booleanSearch(user_query)
+        cosineSimResults = searchEngine.cosineSimilarity(user_query)
+        searchEngine.top5Results(booleanResults, "boolean results")
+        searchEngine.top5Results(cosineSimResults, "cosine similarity results ")
         user_query = input("Enter a query: ").strip()
+
 
 if __name__ == "__main__":
     main()
